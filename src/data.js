19feead7a032bfa23a31eabacf042d9ed3b3898c
Pavel Rodionov <pasha.rod@mail.ru>
2018-10-04 19:15:51 +0300

    rename constant :)

--- src/data.js
+++ src/data.js
@@ -23,12 +23,12 @@ const PreparedData = {
   ROOMS: {MIN: 1, MAX: 5},
   PRICE: {MIN: 1000, MAX: 1000000}
 };
-const DAYS_IN_MS = 24 * 60 * 60 * 1000;
+const MS_IN_DAY = 24 * 60 * 60 * 1000;
 
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const randomElementFromArray = (array) => array[randomNumber(0, array.length - 1)];
 const shuffleArray = (arr) => arr.sort(() => Math.random() - 0.5);
-const dateOffset = (offset) => DAYS_IN_MS * offset;
+const dateOffset = (offset) => MS_IN_DAY * offset;
 
 class Data {
   static generate() {
